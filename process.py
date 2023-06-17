7b26803b0c07a8143e8cc1459de70ea333621dad
Ali Ulaş Hayır <57371604+aliulashayir@users.noreply.github.com>
2023-06-17 20:27:46 +0300

    Add files via upload

--- process.py
+++ process.py
@@ -1,103 +1,102 @@
+import cv2
+import imutils
 from imutils.perspective import four_point_transform
 from skimage.segmentation import clear_border
 import numpy as np
-import imutils
-import cv2
+
+
+def convert_to_gray_and_blur(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 3)
+    return blurred
+
+
+def apply_threshold(blurred):
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    return cv2.bitwise_not(thresh)
+
+
+def find_contours(thresh):
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    return sorted(cnts, key=cv2.contourArea, reverse=True)
+
+
+def find_puzzle_contour(cnts):
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            return approx
+    raise Exception(("Could not find Sudoku puzzle outline."))
+
+
+def apply_perspective_transform(image, gray, puzzleCnt):
+    puzzle = four_point_transform(image, puzzleCnt.reshape(4, 2))
+    warped = four_point_transform(gray, puzzleCnt.reshape(4, 2))
+    return puzzle, warped
 
 
 def find_puzzle(image, debug=False):
-	# convert the image to grayscale and blur
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	blurred = cv2.GaussianBlur(gray, (7, 7), 3) 
+    gray = convert_to_gray_and_blur(image)
+    thresh = apply_threshold(gray)
 
-	#adaptive thresholding and then invert the threshold map
-	thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-	thresh = cv2.bitwise_not(thresh)
+    if debug:
+        cv2.imshow("Puzzle Thresh", thresh)
+        cv2.waitKey(0)
 
+    cnts = find_contours(thresh)
+    puzzleCnt = find_puzzle_contour(cnts)
 
-	if debug:
-		cv2.imshow("Puzzle Thresh", thresh)
-		cv2.waitKey(0)
+    if debug:
+        output = image.copy()
+        cv2.drawContours(output, [puzzleCnt], -1, (0, 255, 0), 2)
+        cv2.imshow("Puzzle Outline", output)
+        cv2.waitKey(0)
 
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-	puzzleCnt = None
-
-	# loop over the contours
-	for c in cnts:
-		# approximate the contour
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-		# if our approximated contour has four points, then we can
-		# assume we have found the outline of the puzzle
-		if len(approx) == 4:
-			puzzleCnt = approx
-			break
-   
-	if puzzleCnt is None:
-		raise Exception(("Could not find Sudoku puzzle outline. "))
-	
-	if debug:
-		output = image.copy()
-		cv2.drawContours(output, [puzzleCnt], -1, (0, 255, 0), 2)
-		cv2.imshow("Puzzle Outline", output)
-		cv2.waitKey(0)
+    return apply_perspective_transform(image, gray, puzzleCnt)
 
 
-	# apply a four point perspective transform to both the original
-	# image and grayscale image to obtain a top-down bird's eye view
-	# of the puzzle
-	puzzle = four_point_transform(image, puzzleCnt.reshape(4, 2))
-	warped = four_point_transform(gray, puzzleCnt.reshape(4, 2))
+def apply_threshold_to_cell(cell):
+    return cv2.threshold(cell, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-	if debug:
-		cv2.imshow("Puzzle Transform", puzzle)
-		cv2.waitKey(0)
-	# return a 2-tuple of puzzle in both RGB and grayscale
-	return (puzzle, warped)
 
+def find_cell_contours(thresh):
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return imutils.grab_contours(cnts)
+
+
+def get_mask(thresh, cnts):
+    c = max(cnts, key=cv2.contourArea)
+    mask = np.zeros(thresh.shape, dtype="uint8")
+    cv2.drawContours(mask, [c], -1, 255, -1)
+    return mask
 
 
 def extract_digit(cell, debug=False):
-	# apply automatic thresholding 
-	thresh = cv2.threshold(cell, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh = clear_border(apply_threshold_to_cell(cell))
 
-	thresh = clear_border(thresh)
-	# check to see if we are visualizing the cell thresholding step
-	if debug:
-		cv2.imshow("Cell Thresh", thresh)
-		cv2.waitKey(0)
+    if debug:
+        cv2.imshow("Cell Thresh", thresh)
+        cv2.waitKey(0)
 
-		
-	# find contours in the thresholded cell
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	# if no contours were found than this is an empty cell
-	if len(cnts) == 0:
-		return None
-	# otherwise, find the largest contour in the cell and create a
-	# mask for the contour
-	c = max(cnts, key=cv2.contourArea)
-	mask = np.zeros(thresh.shape, dtype="uint8")
-	cv2.drawContours(mask, [c], -1, 255, -1)
+    cnts = find_cell_contours(thresh)
 
+    if len(cnts) == 0:
+        return None
 
-	#Percentage of masked pixels relative to the total
+    mask = get_mask(thresh, cnts)
 
-	(h, w) = thresh.shape
-	percentFilled = cv2.countNonZero(mask) / float(w * h)
-	# if less than 2% of the mask is filled then we are looking at
-	# noise and can safely ignore the contour
-	if percentFilled < 0.02:
-		return None
-	# apply the mask to the thresholded cell
-	digit = cv2.bitwise_and(thresh, thresh, mask=mask)
+    (h, w) = thresh.shape
+    percentFilled = cv2.countNonZero(mask) / float(w * h)
 
-	if debug:
-		cv2.imshow("Digit", digit)
-		cv2.waitKey(0)
+    if percentFilled < 0.02:
+        return None
 
-	return digit
+    digit = cv2.bitwise_and(thresh, thresh, mask=mask)
+
+    if debug:
+        cv2.imshow("Digit", digit)
+        cv2.waitKey(0)
+
+    return digit
